24807fed27a61213dcee8a6d8ee0c09a983dc6fe
Benoit Calvez <benoit.calvez@gmail.com>
2012-09-08 12:27:15 +0200

    Update manage.py to use a stock django directory
    It helps people to use django quickly without using pip / virtualenv

--- omgcats/manage.py
+++ omgcats/manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, PROJECT_PATH + "/../src/")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omgcats.settings")
 
     from django.core.management import execute_from_command_line
