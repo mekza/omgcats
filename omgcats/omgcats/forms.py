346673c97bf0e1e02fd4715ef580ca364bbf4fa7
Benoit Calvez <benoit.calvez@gmail.com>
2012-09-10 23:12:35 +0200

    Form customisation: form validation

--- omgcats/omgcats/forms.py
+++ omgcats/omgcats/forms.py
@@ -4,3 +4,25 @@ from django import forms
 class MyFirstForm(forms.Form):
     username = forms.CharField(label='username')
     password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username == 'benoit':
+            raise forms.ValidationError('This username is not valid!')
+
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if not password or not len(password):
+            raise forms.ValidationError('This must be set')
+
+        return password
+
+    def clean(self):
+        data = super(MyFirstForm, self).clean()
+        if data.get('username') == 'ben' and data.get('password') == 'pass':
+            self._errors['password'] = self.error_class(["Invalid password"])
+
+        return data
