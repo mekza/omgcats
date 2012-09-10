bd200f2652438f277767981553ac279f01848019
Benoit Calvez <benoit.calvez@gmail.com>
2012-09-10 22:32:46 +0200

    My first view

--- omgcats/omgcats/urls.py
+++ omgcats/omgcats/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'omgcats.views.home', name='home'),
+    url(r'^$', 'omgcats.views.index', name='index'),
     # url(r'^omgcats/', include('omgcats.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
